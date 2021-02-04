d0a9682eb22734eba1d83ce91938d3c4e7c573f1
adelinel <adeline.latruwe@gmail.com>
2021-02-04 12:50:09 -0800

    added notes services and routes

---
+++ src/notes/notes-router.js
@@ -0,0 +1,75 @@
+const path = require("path");
+const express = require("express");
+const xss = require("xss");
+const NotesService = require("./notes-services");
+
+const notesRouter = express.Router();
+const jsonParser = express.json();
+
+const serializeNote = (note) => ({
+  id: note.id,
+  title: xss(note.title),
+  content: xss(note.content),
+  date_created: note.date_created,
+  folder_id: note.folfer_id,
+});
+
+notesRouter
+  .route("/")
+  .get((req, res, next) => {
+    NotesService.getAllNotes(req.app.get("db"))
+      .then((notes) => {
+        res.json(notes.map(serializeNote));
+      })
+      .catch(next);
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { title, content, folder_id } = req.body;
+    const newNote = { title, content, folder_id };
+
+    // validation
+    for (const [key, value] of Object.entries(newNote)) {
+      if (value == null) {
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` },
+        });
+      }
+    }
+
+    NotesService.insertNote(req.app.get("db"), newNote)
+      .then((note) => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${note.id}`))
+          .json(note);
+      })
+      .catch(next);
+  });
+
+notesRouter
+  .route("/:note_id")
+  .all((req, res, next) => {
+    NotesService.getById(req.app.get("db"), req.params.note_id)
+      .then((note) => {
+        if (!note) {
+          return res.status(404).json({
+            error: { message: `Note doesn't exist` },
+          });
+        }
+        res.note = note; // save the folder for the next middleware
+        next();
+      })
+      .catch(next);
+  })
+  .get((req, res, next) => {
+    res.json(serializeNote(res.note));
+  })
+  .delete((req, res, next) => {
+    NotesService.deleteNote(req.app.get("db"), req.params.note_id)
+      .then(() => {
+        res.status(204).end();
+      })
+      .catch(next);
+  });
+
+module.exports = notesRouter;
