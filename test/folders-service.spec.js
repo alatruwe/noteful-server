f0c37deae8a6fb50a4a297fbbdf853ea38f27f7b
adelinel <adeline.latruwe@gmail.com>
2021-02-05 19:34:06 -0800

    updated tests

--- test/folders-service.spec.js
+++ test/folders-service.spec.js
@@ -11,7 +11,7 @@ describe(`Folders service object`, function () {
   before("make knex instance", () => {
     db = knex({
       client: "pg",
-      connection: process.env.TEST_DB_URL,
+      connection: process.env.TEST_DATABASE_URL,
     });
     // tests skip server.js so we need the instance here
     app.set("db", db);
